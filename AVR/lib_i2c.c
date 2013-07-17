47bf5115c7dc1737c333d647262b696a38bcac8e
James Fowkes <james.fowkes@zipher.co.uk>
2013-07-17 14:05:20 +0100

    Additions to I2C library

--- AVR/lib_i2c.c
+++ AVR/lib_i2c.c
@@ -7,6 +7,10 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#include <stdint.h>
+
+#include "lib_i2c_test.h"
+
 /*
  * Common and Generic Includes
  */
@@ -18,25 +22,15 @@
 enum i2c_state_enum
 {
 	I2CS_IDLE,
-	I2CS_REQ,
-	I2CS_STARTED,
+	I2CS_STARTING,
 	I2CS_ADDRESSING,
 	I2CS_TRANSFERRING,
-}
-typedef enum i2c_state_enum I2C_STATE_ENUM;
-
-enum i2c_event_enum
-{
-	I2CE_REQ,
-	I2CE_START,
-	I2CE_SLA_ACK,
-	I2CE_DATA_ACK,
-	I2CE_EOD
 };
-typedef enum i2c_event_enum I2C_EVENT_ENUM;
+typedef enum i2c_state_enum I2C_STATE_ENUM;
 
 enum i2c_mode_enum
 {
+	I2CM_NONE,
 	I2CM_MT,
 	I2CM_MR,
 	I2CM_ST,
@@ -44,147 +38,386 @@ enum i2c_mode_enum
 };
 typedef enum i2c_mode_enum I2C_MODE_ENUM;
 
-typedef void (*i2c_sm_func)(void);
+typedef void (*i2c_sm_func)(uint8_t);
 
 struct i2c_statemachineentry_struct
 {
-	I2C_STATE_ENUM	state;
-	I2C_EVENT_ENUM	event;
+	I2C_STATE_ENUM	currentState;
+	uint8_t			event;
 	i2c_sm_func		handler;
 	I2C_STATE_ENUM	nextState;
 };
 typedef struct i2c_statemachineentry_struct I2C_STATEMACHINEENTRY;
 
-static I2C_MODE_ENUM mode = I2CM_MT;
-static I2C_STATE_ENUM state = I2C_IDLE;
-static I2C_TRANSFER_DATA currentTransferData;
+struct i2c_statemachine_struct
+{
+	bool busy;
+	I2C_STATEMACHINEENTRY* entries;
+};
+typedef struct i2c_statemachine_struct I2C_STATEMACHINE;
+
+static bool startMaster(I2C_TRANSFER_DATA * newTransferData, I2C_MODE_ENUM selected_mode);
+
+static void startSR(uint8_t event);
+static void startST(uint8_t event);
+
+static void errorCondition(uint8_t event);
+static void sendAddress(uint8_t event);
+static void txNextByte(uint8_t event);
+static void rxDATA(uint8_t event);
+static void rxSLA(uint8_t event);
+
+static void sendAck(uint8_t event);
+static void sendNack(uint8_t event);
+static void ack_or_nack_on_data_length(void);
+
+static void ack();
+static void nack();
+static void stop();
+static void release();
+
+static void finishTx();
+static void finishRx(uint8_t event);
+
+static bool bufferFull(void);
+
+static void i2c_sm_event(uint8_t event);
+
+static I2C_STATEMACHINEENTRY sm_emtries[] = 
+{
+	{I2CS_IDLE,			TW_ST_SLA_ACK,				startST,		I2CS_TRANSFERRING	},
+	{I2CS_IDLE,			TW_ST_ARB_LOST_SLA_ACK,		startST,		I2CS_TRANSFERRING	},
+		
+	{I2CS_IDLE,			TW_SR_SLA_ACK,				startSR,		I2CS_TRANSFERRING	},
+	{I2CS_IDLE,			TW_SR_GCALL_ACK,			startSR,		I2CS_TRANSFERRING	},
+	{I2CS_IDLE,			TW_SR_ARB_LOST_SLA_ACK,		startSR,		I2CS_TRANSFERRING	},
+	{I2CS_IDLE,			TW_SR_ARB_LOST_GCALL_ACK,	startSR,		I2CS_TRANSFERRING	},
+	
+	{I2CS_IDLE,			TW_START,					sendAddress,	I2CS_ADDRESSING		},
+	{I2CS_IDLE,			TW_BUS_ERROR,				errorCondition,	I2CS_IDLE			},
+		
+	{I2CS_IDLE,			TW_BUS_ERROR,				errorCondition,	I2CS_IDLE			},
+				
+	{I2CS_ADDRESSING,	TW_MT_SLA_ACK,				txNextByte,		I2CS_TRANSFERRING	},
+	{I2CS_ADDRESSING,	TW_MT_SLA_NACK,				errorCondition,	I2CS_IDLE			},
+			
+	{I2CS_ADDRESSING,	TW_MR_SLA_ACK,				rxSLA,			I2CS_TRANSFERRING	},
+	{I2CS_ADDRESSING,	TW_MR_SLA_NACK,				rxSLA,			I2CS_TRANSFERRING	},
+			
+	{I2CS_ADDRESSING,	TW_BUS_ERROR,				errorCondition,	I2CS_IDLE			},
+			
+	{I2CS_TRANSFERRING,	TW_MT_DATA_ACK,				txNextByte,		I2CS_TRANSFERRING	},
+	{I2CS_TRANSFERRING,	TW_MT_DATA_NACK,			errorCondition,	I2CS_IDLE			},
+	{I2CS_TRANSFERRING,	TW_MT_ARB_LOST,				errorCondition,	I2CS_IDLE			},
+			
+	{I2CS_TRANSFERRING,	TW_MR_DATA_ACK,				rxDATA,			I2CS_TRANSFERRING	},
+	{I2CS_TRANSFERRING,	TW_MR_DATA_NACK,			rxDATA,			I2CS_TRANSFERRING	},
+	{I2CS_TRANSFERRING,	TW_MR_ARB_LOST,				errorCondition,	I2CS_IDLE			},
+			
+	{I2CS_TRANSFERRING,	TW_ST_DATA_ACK,				txNextByte,		I2CS_TRANSFERRING	},
+	{I2CS_TRANSFERRING,	TW_ST_DATA_NACK,			sendAck,		I2CS_IDLE			},
+	{I2CS_TRANSFERRING,	TW_ST_LAST_DATA,			sendAck,		I2CS_IDLE			},
+		
+	{I2CS_TRANSFERRING,	TW_SR_DATA_ACK,				rxSLA,			I2CS_TRANSFERRING	},
+	{I2CS_TRANSFERRING,	TW_SR_GCALL_DATA_ACK,		rxSLA,			I2CS_TRANSFERRING	},
+	{I2CS_TRANSFERRING,	TW_SR_DATA_NACK,			sendNack,		I2CS_TRANSFERRING	},
+	{I2CS_TRANSFERRING,	TW_SR_GCALL_DATA_NACK,		sendNack,		I2CS_TRANSFERRING	},
+	{I2CS_TRANSFERRING,	TW_SR_STOP,					finishRx,		I2CS_IDLE			},
+		
+	{I2CS_TRANSFERRING,	TW_BUS_ERROR,				errorCondition,	I2CS_IDLE			},
+};
+
+static I2C_STATEMACHINE sm = {false, sm_emtries};
+
+static I2C_MODE_ENUM mode = I2CM_NONE;
+static I2C_STATE_ENUM state = I2CS_IDLE;
+
+static I2C_CONDITION_ENUM transferCondition = I2CC_CONTINUE;
+
+static I2C_TRANSFER_DATA * transferData;
+
+static I2C_SLAVE_HANDLER slaveRxHandler;
+static I2C_SLAVE_HANDLER slaveTxHandler;
 
 static uint8_t byteCount = 0;
 
-static I2C_STATEMACHINEENTRY sm[] = 
+bool I2C_Init(I2C_SLAVE_HANDLER srxHandler, I2C_SLAVE_HANDLER stxHandler)
 {
-	{I2CS_IDLE,			I2CE_MT_REQ,	sendStart,		I2CS_REQ			},
-	
-	{I2CS_REQ,			I2CS_START,		sendAddress,	I2CS_ADDRESSING		},
-	
-	{I2CS_ADDRESSING	I2CS_SLA_ACK	txrxNextByte,	I2CS_TRANSFERRING	},
-	
-	{I2CS_TRANSFERRING,	I2CE_DATA_ACK	txrxNextByte,	I2CS_TRANSFERRING	},
-	{I2CS_TRANSFERRING,	I2CE_ERROR		errorCondition,	I2CS_IDLE			},
-	{I2CS_TRANSFERRING,	I2CE_EOD,		finishTxRx,		I2CS_IDLE			},
-}
-
-bool I2C_Init(void)
-{
+	slaveRxHandler = srxHandler;
+	slaveTxHandler = stxHandler;
 	return true;
 }
 
-bool I2C_MasterWrite(I2C_TRANSFER_DATA * transferData)
+bool I2C_MasterWrite(I2C_TRANSFER_DATA * newTransferData)
+{
+	return startMaster(newTransferData, I2CM_MT);
+}
+
+bool I2C_MasterRead(I2C_TRANSFER_DATA * newTransferData)
+{
+	return startMaster(newTransferData, I2CM_MT);
+}
+
+static bool startMaster(I2C_TRANSFER_DATA * newTransferData, I2C_MODE_ENUM selected_mode)
 {
 	bool success = true;
-	if (transferData)
+	if (newTransferData)
 	{
-		if(transferData.buffer && transferData.byteCount)
+		if(newTransferData->buffer && newTransferData->byteCount)
 		{
-			currentTransferData = transferData;
+			// Start in MT or MR mode. First response will be TW_START
+			transferData = newTransferData;
+			transferCondition = I2CC_CONTINUE;
+			mode = selected_mode;
 			byteCount = 0;
-			mode = I2CM_MT;
-			twi_sm_event(I2CE_MT_REQ);
+			TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
 		}
 	}
 	return success;
+
 }
 
-bool I2C_MasterRead(I2C_TRANSFER_DATA * transferData)
+static void startST(uint8_t event)
 {
-	bool success = true;
-	if (transferData)
-	{
-		if(transferData.buffer && transferData.byteCount)
-		{
-			currentTransferData = transferData;
-			byteCount = 0;
-			mode = I2CM_MR;
-			twi_sm_event(I2CE_MR_REQ);
-		}
-	}
-	return success;
+	(void)event;
+	mode = I2CM_ST;
+	transferCondition = I2CC_CONTINUE;
+	byteCount = 0;
+	
+	transferData->buffer = NULL;
+	transferData->byteCount = 0;
+	slaveTxHandler(transferData);
+	
+	// Act like we got a data ACK
+	txNextByte(TW_ST_DATA_ACK);
 }
 
-void sendStart(void)
+static void startSR(uint8_t event)
 {
-	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
+	(void)event;
+	mode = I2CM_SR;
+	transferCondition = I2CC_CONTINUE;
+	byteCount = 0;
+	ack();
 }
 
-void sendAddress(void)
+static void sendAddress(uint8_t event)
 {
-	address = (currentTransferData.address << 1);
+	(void)event;
+	uint8_t address = (transferData->address << 1);
 	address |= (mode == I2CM_MR) ? 0x01 : 0x00;
 	
 	TWDR = address;
-	TWCR = (1<<TWINT) | (1<<TWEN);
+	ack();
 }
 
-void txrxNextByte(void)
+static void txNextByte(uint8_t event)
 {
-	if (byteCount < (transferData.bytes - 1))
+	(void)event;
+	if (!bufferFull())
 	{
-		switch (mode)
-		{
-		case I2CM_MT:
-			TWDR = transferData.buffer[byteCount++];
-			TWCR = (1<<TWINT) | (1<<TWEN);
-			break;
-		case I2CM_MR:
-			transferData.buffer[byteCount++] = TWDR;
-			if (byteCount < (transferData.bytes - 1))
-			{
-				ack();
-			}
-			else
-			{
-				nack()
-			}
-			break;
-		case I2CM_ST:
-			break;
-		case I2CM_SR:
-			break;
-		}
-		
-		byteCount++;
+		TWDR = transferData->buffer[byteCount++]; // More data to send
+		ack();
 	}
 	else
 	{
-		i2c_sm_event(I2CE_EOD);
+		finishTx(); // No more data to send
 	}
 }
 
-void errorCondition(void)
+static void rxDATA(uint8_t event)
 {
-	stop(void);
-	transferData.callback(false);
+	switch (mode)
+	{
+	case I2CM_MR:
+		// If this is a data ack/nack, store the next byte
+		switch (event)
+		{
+		case TW_MR_DATA_ACK:
+			transferData->buffer[byteCount++] = TWDR;
+			ack_or_nack_on_data_length();
+			break;
+		case TW_MR_DATA_NACK:
+			transferData->buffer[byteCount++] = TWDR;
+			errorCondition(TW_MR_DATA_NACK);
+			break;
+		default:
+			break;
+		}
+	case I2CM_SR:
+		// Store the data, then ack/nack
+		transferData->buffer[byteCount++] = TWDR;
+		ack_or_nack_on_data_length();
+		break;
+	default:
+		break;
+	}
 }
 
-void finishTxRx(void)
+static void rxSLA(uint8_t event)
 {
-	stop(void);
-	transferData.callback(true);
+	switch (mode)
+	{
+	case I2CM_MR:
+		// If this is a data ack/nack, store the next byte
+		switch (event)
+		{
+		case TW_MR_SLA_ACK:
+			ack_or_nack_on_data_length();
+			break;
+		case TW_MR_SLA_NACK:
+			stop();
+			break;
+		default:
+			break;
+		}
+	case I2CM_SR:
+		// Store the data, then ack/nack
+		transferData->buffer[byteCount++] = TWDR;
+		ack_or_nack_on_data_length();
+		break;
+	default:
+		break;
+	}
 }
 
-void ack(void)
+static void sendAck(uint8_t event)
+{
+	(void)event;
+	ack();
+}
+
+static void sendNack(uint8_t event)
+{
+	(void)event;
+	nack();
+}
+
+static void ack_or_nack_on_data_length(void)
+{
+	if (!bufferFull())
+	{
+		ack(); //ACK if more bytes to come
+	}
+	else
+	{
+		nack();
+	}
+}
+
+static bool bufferFull(void)
+{
+	return (byteCount == (transferData->byteCount - 1));
+}
+
+static void finishTx(void)
+{
+	switch(mode)
+	{
+	case I2CM_MT:
+		stop();
+		break;
+	case I2CM_ST:
+		ack_or_nack_on_data_length();
+		break;
+	default:
+		break;
+	}
+	
+	transferCondition = I2CC_SUCCESS;
+}
+
+static void finishRx(uint8_t event)
+{
+	(void)event;
+	switch(mode)
+	{
+	case I2CM_MR:
+		stop();
+		break;
+	case I2CM_SR:
+		stop();
+		release();
+		break;
+	default:
+		break;
+	}
+	
+	transferCondition = I2CC_SUCCESS;
+}
+
+static void errorCondition(uint8_t event)
+{
+	switch(event)
+	{
+	case TW_MT_SLA_NACK:
+	case TW_MT_DATA_NACK:
+	case TW_MR_SLA_NACK:
+		stop();
+		break;
+	case TW_MT_ARB_LOST:
+		release();
+		break;
+	default:
+		break;
+	}
+	
+	transferCondition = I2CC_FAILED;
+}
+
+static void ack(void)
 {
 	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWEA);
 }
 
-void nack(void)
+static void nack(void)
 {
 	TWCR = (1<<TWINT) | (1<<TWEN);
 }
 
-void stop(void)
+static void stop(void)
 {
 	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);
+	mode = I2CM_NONE;
+}
+
+static void release(void)
+{
+	TWCR = (1<<TWEN) | (1<<TWIE) | (1<<TWEA) | (1<<TWINT);
+	mode = I2CM_NONE;
+}
+
+static void i2c_sm_event(uint8_t event)
+{
+	sm.busy = true;
+	
+	I2C_STATEMACHINEENTRY *search = sm.entries;
+	
+	// Find state
+	while (search->currentState != state)
+	{
+		search++;
+	}
+	
+	while (search->currentState == state)
+	{
+		if (search->event == event) {break;}
+	}
+	
+	if (search->event == event)
+	{
+		search->handler(event);
+		if (I2CC_CONTINUE == transferCondition)
+		{
+			state = search->nextState;
+		}
+		else
+		{
+			transferData->callback(transferCondition);
+		}
+	}
+	
+	sm.busy = false;
 }
 
 ISR(TWI_vect)
@@ -192,20 +425,12 @@ ISR(TWI_vect)
 	uint8_t status = TWSR;
 	if (status & TWINT)
 	{
-		switch(status & 0xF8)
+		status = status & 0xF8;
+		
+		if (status != TW_NO_INFO)
 		{
-		case TW_START:
-			i2c_sm_event(I2CE_START);
-			break;
-		case TW_MT_SLA_ACK:
-			i2c_sm_event(I2CE_SLA_ACK);
-			break;
-		case TW_MT_DATA_ACK:
-		case TW_MR_DATA_ACK:
-			i2c_sm_event(I2CE_DATA_ACK);
-			break;
-		default:
-			i2c_sm_event(I2CE_ERROR);
+			while (sm.busy) {continue;}
+			i2c_sm_event(status);
 		}
 	}
 }
--- Common/lib_i2c_common.h
+++ Common/lib_i2c_common.h
@@ -1,22 +1,30 @@
 #ifndef _LIB_I2C_H_
 #define _LIB_I2C_H_
 
-typedef void (*I2CCALLBACK)(bool success);
+typedef void (*I2C_CALLBACK)(bool success);
 
 struct i2c_transfer_data_struct
 {
 	uint8_t			address;
 	uint8_t			*buffer;
 	uint8_t			byteCount;
-	I2CCALLBACK		callback;
+	I2C_CALLBACK	callback;
 };
 typedef struct i2c_transfer_data_struct I2C_TRANSFER_DATA;
 
-bool I2C_Init(void);
+enum i2c_condition_enum
+{
+	I2CC_CONTINUE,
+	I2CC_FAILED,
+	I2CC_SUCCESS,
+};
+typedef enum i2c_condition_enum I2C_CONDITION_ENUM;
+
+typedef void (*I2C_SLAVE_HANDLER)(I2C_TRANSFER_DATA * transfer);
+
+bool I2C_Init(I2C_SLAVE_HANDLER srxHandler, I2C_SLAVE_HANDLER stxHandler);
 
 bool I2C_MasterWrite(I2C_TRANSFER_DATA * transfer);
 bool I2C_MasterRead(I2C_TRANSFER_DATA * transfer);
-bool I2C_SlaveWrite(I2C_TRANSFER_DATA * transfer);
-bool I2C_SlaveRead(I2C_TRANSFER_DATA * transfer);
 
 #endif
