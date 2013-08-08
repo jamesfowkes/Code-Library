61ed96208f96dafc3f47521276566bb582a1bd05
jamesfowkes <jamesfowkes@gmail.com>
2013-08-08 22:46:15 +0100

    Added utility macros header

---
+++ Utility/util_macros.h
@@ -0,0 +1,10 @@
+#ifndef _UTIL_MACROS_H_
+#define _UTIL_MACROS_H_
+
+#define min(a,b) (((a) < (b)) ? (a) : (b))
+
+#define max(a,b) (((a) > (b)) ? (a) : (b))
+
+#define inrange(n, lo, hi) ((lo <= n) && (hi >= n))
+
+#endif
