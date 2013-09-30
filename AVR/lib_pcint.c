5746d9ea94094a44b8422093026db64e2f0602ad
James Fowkes <james.fowkes@zipher.co.uk>
2013-09-30 18:00:57 +0100

    Added interrupt vectors to pcint library

--- AVR/lib_pcint.c
+++ AVR/lib_pcint.c
@@ -5,6 +5,12 @@
 #include <stdbool.h>
 
 /*
+ * Defines and Typedefs
+ */
+
+#define PCINT_ISR(name) ISR(name ## _vect) { s_bTriggered[e ## name] = true; }
+
+/*
  * AVR Includes (Defines and Primitives)
  */
 #include <avr/io.h>
@@ -16,11 +22,51 @@
  
 #include "lib_pcint.h"
 
-bool PCINT_TestAndClear(PCINT_FLAG * flag)
+static bool s_bTriggered[ePCINT_MAX];
+
+bool PCINT_TestAndClear(PCINT_ENUM eInterrupt)
 {
 	cli();
-	bool complete = true; // TODO: Get actual interrupt value
-	*flag = false;
+	bool complete = s_bTriggered[eInterrupt];
+	s_bTriggered[eInterrupt] = false;
 	sei();
 	return complete;
 }
+
+#ifdef INT0_vect
+PCINT_ISR(INT0);
+#endif
+#ifdef INT1_vect
+PCINT_ISR(INT1);
+#endif
+#ifdef INT2_vect
+PCINT_ISR(INT2);
+#endif
+#ifdef INT3_vect
+PCINT_ISR(INT3);
+#endif
+#ifdef INT4_vect
+PCINT_ISR(INT4);
+#endif
+#ifdef INT5_vect
+PCINT_ISR(INT5);
+#endif
+#ifdef INT6_vect
+PCINT_ISR(INT6);
+#endif
+#ifdef INT7_vect
+PCINT_ISR(INT7);
+#endif
+
+#ifdef PCINT0_vect
+PCINT_ISR(PCINT0);
+#endif
+#ifdef PCINT1_vect
+PCINT_ISR(PCINT1);
+#endif
+#ifdef PCINT2_vect
+PCINT_ISR(PCINT2);
+#endif
+#ifdef PCINT3_vect
+PCINT_ISR(PCINT3);
+#endif
--- AVR/lib_pcint.h
+++ AVR/lib_pcint.h
@@ -3,6 +3,28 @@
 
 typedef bool PCINT_FLAG;
 
-bool PCINT_TestAndClear(PCINT_FLAG * flag);
+enum pcint_enum
+{
+	eINT0,
+	eINT1,
+	eINT2,
+	eINT3,
+	eINT4,
+	eINT5,
+	eINT6,
+	eINT7,
+	ePCINT0,
+	ePCINT1,
+	ePCINT2,
+	ePCINT3,
+	ePCINT4,
+	ePCINT5,
+	ePCINT6,
+	ePCINT7,
+	ePCINT_MAX
+};
+typedef enum pcint_enum PCINT_ENUM;
+
+bool PCINT_TestAndClear(PCINT_ENUM eInterrupt);
 
 #endif
