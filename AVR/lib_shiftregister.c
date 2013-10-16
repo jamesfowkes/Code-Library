3a2e39cb9d7dcef248ecf36228d027598989d583
jamesfowkes <jamesfowkes@gmail.com>
2013-10-16 20:47:19 +0100

    Added direction selection to shift register

--- AVR/lib_shiftregister.c
+++ AVR/lib_shiftregister.c
@@ -31,23 +31,40 @@ void SR_Init(volatile uint8_t * clockPort, uint8_t clockPin, volatile uint8_t * 
 	s_dataPin = dataPin;
 }
 
-void SR_ShiftOut(uint8_t* data, uint8_t nBytes, SR_CLKEDGE_ENUM edge)
+void SR_ShiftOut(uint8_t* data, uint8_t nBytes, SR_CLKEDGE_ENUM edge, SR_DIRECTION_ENUM dir)
 {
 	uint8_t b = 0;
 	uint8_t mask;
 	
 	for (; b < nBytes; ++b)
 	{
-		for (mask = 0x80; mask; mask >>= 1)
+		if (dir == SR_DIR_MSBFIRST)
 		{
-			// Clear the clock
-			(edge != SR_CLKEDGE_POS) ? (*s_clockPort |= (1 << s_clockPin)) : (*s_clockPort &= ~(1 << s_clockPin));
-			
-			// Setup data
-			((data[b] & mask) == mask) ? (*s_dataPort |= (1 << s_dataPin)) : (*s_dataPort &= ~(1 << s_dataPin));
-			
-			// Assert the clock
-			(edge == SR_CLKEDGE_POS) ? (*s_clockPort |= (1 << s_clockPin)) : (*s_clockPort &= ~(1 << s_clockPin));
+			for (mask = 0x80; mask; mask >>= 1)
+			{
+				// Clear the clock
+				(edge != SR_CLKEDGE_POS) ? (*s_clockPort |= (1 << s_clockPin)) : (*s_clockPort &= ~(1 << s_clockPin));
+
+				// Setup data
+				((data[b] & mask) == mask) ? (*s_dataPort |= (1 << s_dataPin)) : (*s_dataPort &= ~(1 << s_dataPin));
+
+				// Assert the clock
+				(edge == SR_CLKEDGE_POS) ? (*s_clockPort |= (1 << s_clockPin)) : (*s_clockPort &= ~(1 << s_clockPin));
+			}
+		}
+		else
+		{
+			for (mask = 0x01; mask; mask <<= 1)
+			{
+				// Clear the clock
+				(edge != SR_CLKEDGE_POS) ? (*s_clockPort |= (1 << s_clockPin)) : (*s_clockPort &= ~(1 << s_clockPin));
+
+				// Setup data
+				((data[b] & mask) == mask) ? (*s_dataPort |= (1 << s_dataPin)) : (*s_dataPort &= ~(1 << s_dataPin));
+
+				// Assert the clock
+				(edge == SR_CLKEDGE_POS) ? (*s_clockPort |= (1 << s_clockPin)) : (*s_clockPort &= ~(1 << s_clockPin));
+			}
 		}
 	}
 }
--- AVR/lib_shiftregister.h
+++ AVR/lib_shiftregister.h
@@ -2,6 +2,6 @@
 #define _LIB_SHIFTREGISTER_H_
 
 void SR_Init(volatile uint8_t * clockPort, uint8_t clockPin, volatile uint8_t * dataPort, uint8_t dataPin);
-void SR_ShiftOut(uint8_t* data, uint8_t nBytes, SR_CLKEDGE_ENUM edge);
+void SR_ShiftOut(uint8_t* data, uint8_t nBytes, SR_CLKEDGE_ENUM edge, SR_DIRECTION_ENUM dir);
 
 #endif
