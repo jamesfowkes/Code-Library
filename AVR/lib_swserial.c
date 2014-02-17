580234a65aa49e2cab7684d811622788c0031f37
James Fowkes <jamesfowkes@gmail.com>
2014-02-17 18:50:03 +0000

    Added constant delay for SW serial instructions

--- AVR/lib_swserial.c
+++ AVR/lib_swserial.c
@@ -55,7 +55,7 @@ enum
 
 #define START_CONDITION (true)
 
-#define delay() _delay_loop_2(s_DelayCount)
+#define delay() _delay_loop_2(s_DelayCount - 6)
 
 #define US_TO_DELAYCOUNTS(us)	div_round(F_CPU * (us), 4000000 + s_uCPUDiv)
 
