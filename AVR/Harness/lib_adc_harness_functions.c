e901cb4afd6122de4464343cb21cdfe6e56a750e
James Fowkes <james.fowkes@zipher.co.uk>
2014-03-12 10:46:32 +0000

    ADC harness used sequence generator

--- AVR/Harness/lib_adc_harness_functions.c
+++ AVR/Harness/lib_adc_harness_functions.c
@@ -7,26 +7,20 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#include "util_sequence_generator.h"
 #include "lib_adc.h"
 #include "lib_adc_harness_functions.h"
 
-static char * arrayPtrs[LIB_ADC_CH_MAX];
-static uint16_t readIndexes[LIB_ADC_CH_MAX];
-static uint16_t arrayLengths[LIB_ADC_CH_MAX];
+static SEQUENCE * seqPtrs[LIB_ADC_CH_MAX];
 
-void ADC_Harness_SetReadingArray(LIB_ADC_CHANNEL_ENUM eChannel, uint16_t * array, uint16_t length)
+void ADC_Harness_SetReadingArray(LIB_ADC_CHANNEL_ENUM eChannel, SEQUENCE * seq)
 {
-	arrayPtrs[eChannel] = array;
-	readIndexes[eChannel] = 0;
-	arrayLengths[eChannel] = length;
+	seqPtrs[eChannel] = seq;
 }
 
 void ADC_Harness_TriggerISR(ADC_CONTROL_ENUM * adc)
 {
-	adc->reading = arrayPtrs[adc->channel][ readIndexes[adc->channel] ];
+	adc->reading = SEQGEN_Read ( seqPtrs[adc->channel] );
 	adc->conversionComplete = true;
 	adc->busy = false;
-	
-	incrementwithrollover(readIndexes[adc->channel], arrayLengths[LIB_ADC_CH_MAX] - 1);
-
 }
--- AVR/Harness/lib_adc_harness_functions.h
+++ AVR/Harness/lib_adc_harness_functions.h
@@ -2,6 +2,6 @@
 #define _LIB_ADC_HARNESS_H_
 
 void ADC_Harness_TriggerISR(ADC_CONTROL_ENUM * adc);
-void ADC_Harness_SetReadingArray(LIB_ADC_CHANNEL_ENUM eChannel, uint16_t * array, uint16_t length);
+void ADC_Harness_SetReadingArray(LIB_ADC_CHANNEL_ENUM eChannel, SEQUENCE * seq);
 
 #endif
