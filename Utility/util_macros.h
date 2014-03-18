b05f871b69f91368de147052bdcf89a5aad7b0f0
James Fowkes <james.fowkes@zipher.co.uk>
2014-03-18 14:57:54 +0000

    Correct rounded int division for -ve numbers

--- Utility/util_macros.h
+++ Utility/util_macros.h
@@ -15,6 +15,9 @@
 #define set(reg, bit) (reg |= (1 << bit))
 #define clr(reg, bit) (reg &= ~(1 << bit))
 
-#define div_round(x, y) (((x) + ((y)/2))/(y))
+#define div_round_pos(x, y) (((x) + ((y)/2))/(y))
+#define div_round_neg(x, y) (((x) - ((y)/2))/(y))
+
+#define div_round(x, y) (x >= 0) ? div_round_pos(x, y) : div_round_neg(x, y)
 
 #endif
