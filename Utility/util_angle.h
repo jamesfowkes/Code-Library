3b9b58ca39d81f78ffc43b5474845a1c011aec67
James Fowkes <jamesfowkes@gmail.com>
2014-04-14 23:39:39 +0100

    Added C++ guards to util_angle

--- Utility/util_angle.h
+++ Utility/util_angle.h
@@ -1,10 +1,18 @@
 #ifndef _UTIL_ANGLE_H_
 #define _UTIL_ANGLE_H_
 
+#ifdef __cplusplus
+extern "C"{
+#endif
+
 uint16_t reciprocal_deg(int16_t a);
 uint16_t reciprocal_tdeg(int16_t a);
 
 uint16_t mirror_deg(int16_t a, int16_t mirror);
 uint16_t mirror_tdeg(int16_t a, int16_t mirror);
+
+#ifdef __cplusplus
+};
+#endif
 
 #endif
