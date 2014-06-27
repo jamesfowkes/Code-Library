008d9f5ee28479dd9c968dced13d3488342e042e
James Fowkes <james.fowkes@videojet.com>
2014-06-27 14:46:37 +0100

    Added program memory placement header for harness

---
+++ Utility/harness_memory_placement.h
@@ -0,0 +1,12 @@
+#ifndef _HARNESS_MEMORY_PLACEMENT_H_
+#define _HARNESS_MEMORY_PLACEMENT_H_
+
+/* This should not be directly included by applications!
+Use the generic util_memory_placement.h instead! */
+
+#define pgm_read_byte(x) (*x)
+#define IN_PMEM(x) x
+#define	PMEM_STRNCPY strncpy
+#define STRNCMP strncmp
+
+#endif
