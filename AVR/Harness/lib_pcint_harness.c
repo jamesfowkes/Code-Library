03ec76a94ab24817572c485b627a1fff40cfa409
James Fowkes <james.fowkes@videojet.com>
2014-06-30 12:38:37 +0100

    Added PCINT test harness

---
+++ AVR/Harness/lib_pcint_harness.c
@@ -0,0 +1,13 @@
+/*
+ * Standard Library Includes
+ */
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <assert.h>
+
+uint8_t PCICR;
+uint8_t PCMSK0;
+uint8_t PCMSK1;
+uint8_t PCMSK2;
