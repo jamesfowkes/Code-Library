34864f2e3a83961a4ca48c41078cd21d100353fa
James Fowkes <james.fowkes@videojet.com>
2014-07-04 15:00:33 +0100

    Refactored utility tests

--- Utility/Tests/util_geo/util_geo_test.mk
+++ Utility/Tests/util_geo/util_geo_test.mk
@@ -9,46 +9,7 @@
 #   [Released under MIT License. Please refer to license.txt for details]
 # ========================================== 
 
-CC=gcc
-UNITY_FOLDER=E:/WorkDir/Unity
-LIBS_DIR=$(PROJECTS_PATH)/Libs
-UTIL_DIR=$(LIBS_DIR)/Utility
-TEST_DIR=$(UTIL_DIR)/Tests/$(TEST_NAME)
+EXTRA_SRC_FILES = \
+	$(UTIL_DIR)/util_angle.c \
 
-DEL = python $(LIBS_DIR)/del.py
-
-ifeq ($(OS),Windows_NT)
-	TARGET_EXTENSION=.exe
-else
-	TARGET_EXTENSION=.out
-endif
-
-TARGET = $(TEST_NAME)_test$(TARGET_EXTENSION)
-
-SRC_FILES= \
-$(UNITY_FOLDER)/src/unity.c \
-$(UTIL_DIR)/$(TEST_NAME).c \
-$(UTIL_DIR)/util_angle.c \
-$(TEST_DIR)/$(TEST_NAME)_test.c \
-$(TEST_DIR)/$(TEST_NAME)_test_Runner.c
-
-OBJS = $(SRC_FILES:.c=.o)
-
-INCLUDES = -I$(UNITY_FOLDER)/src -I$(UNITY_FOLDER)/extras/fixture/src -I$(UTIL_DIR)
-CCFLAGS = -Wall -Wextra -DTEST_HARNESS -DUNITY_TEST -std=c99 -DUNIX_TIME_TYPE=int32_t
-
-CLEANUP = $(DEL) $(TARGET) $(OBJS)
-
-%.o:%.c
-	$(CC) $(INCLUDES) $(CCFLAGS) -c $< -o $@
-	
-all: clean $(TARGET)
-
-$(TARGET): $(OBJS)
-	$(CC) $(CCFLAGS) $(LDFLAGS) -o ./$(TARGET) $^
-	./$(TARGET)
-
-clean:
-	$(CLEANUP)
-	
-
+EXTRA_CCFLAGS = -DUNIX_TIME_TYPE=int32_t
--- Utility/Tests/util_sequence_generator/util_sequence_generator_test.mk
+++ Utility/Tests/util_sequence_generator/util_sequence_generator_test.mk
@@ -9,47 +9,10 @@
 #   [Released under MIT License. Please refer to license.txt for details]
 # ========================================== 
 
-CC=gcc
-UNITY_FOLDER=E:/WorkDir/Unity
-LIBS_DIR=$(PROJECTS_PATH)/Libs
-UTIL_DIR=$(LIBS_DIR)/Utility
-TEST_DIR=$(UTIL_DIR)/Tests/$(TEST_NAME)
+EXTRA_SRC_FILES = \
+	$(LIBS_DIR)/Generics/memorypool.c
 
-DEL = python $(LIBS_DIR)/del.py
+EXTRA_CCFLAGS = -DMEMORY_POOL_BYTES=5096
 
-ifeq ($(OS),Windows_NT)
-	TARGET_EXTENSION=.exe
-else
-	TARGET_EXTENSION=.out
-endif
-
-TARGET = $(TEST_NAME)$(TARGET_EXTENSION)
-
-SRC_FILES= \
-$(UNITY_FOLDER)/src/unity.c \
-$(UTIL_DIR)/$(TEST_NAME).c \
-$(TEST_DIR)/$(TEST_NAME)_test.c \
-$(TEST_DIR)/$(TEST_NAME)_test_Runner.c \
-$(LIBS_DIR)/Generics/memorypool.c
-
-OBJS = $(SRC_FILES:.c=.o)
-
-INCLUDES=-I$(UNITY_FOLDER)/src -I$(UNITY_FOLDER)/extras/fixture/src -I$(UTIL_DIR) -I$(LIBS_DIR)/Generics
-CCFLAGS=-DTEST_HARNESS -DUNITY_TEST -std=c99 -DMEMORY_POOL_BYTES=5096
-
-CLEANUP = $(DEL) $(TARGET) $(OBJS)
-
-%.o:%.c
-	$(CC) $(INCLUDES) $(CCFLAGS) -c $< -o $@
-	
-all: clean $(TARGET)
-
-$(TARGET): $(OBJS)
-	$(CC) $(CCFLAGS) $(LDFLAGS) -o ./$(TARGET) $^
-	./$(TARGET)
-
-clean:
-	$(CLEANUP)
-	
-
-
+EXTRA_INCLUDES = \
+	-I$(LIBS_DIR)/Generics
