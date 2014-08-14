78eb7d0ba2884f1df6f6944e6fff77aa04a9b38e
James Fowkes <james.fowkes@videojet.com>
2014-08-14 14:04:07 +0100

    Added include guards for Arduino use of utility libs

--- Utility/util_angle.h
+++ Utility/util_angle.h
@@ -11,8 +11,10 @@ uint16_t reciprocal_tdeg(int16_t a);
 uint16_t mirror_deg(int16_t a, int16_t mirror);
 uint16_t mirror_tdeg(int16_t a, int16_t mirror);
 
+#ifndef ARDUINO
 float radians(float deg);
 float degrees(float rad);
+#endif
 
 #ifdef __cplusplus
 };
